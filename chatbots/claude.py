6274fbab9696c90ca923d9747318dd3a6d34d7f8
harshita999 <91416411+harshita999@users.noreply.github.com>
2024-10-02 23:21:51 +0530

    Add files via upload

---
+++ chatbots/claude.py
@@ -0,0 +1,29 @@
+import boto3
+import json
+
+prompt_data = """
+Write a poem on Machine Learning
+
+"""
+
+bedrock= boto3.client(service_name="bedrock-runtime")
+
+payload= {
+    "prompt":prompt_data,
+    "maxTokens":512,
+    "temperature":0.8,
+    "topP":0.8
+}
+
+body= json.dumps(payload)
+model_id = "anthropic.claude-v2"
+response= bedrock.invoke_model(
+    body=body,
+    modelId= model_id,
+    accept= "application/json",
+    contentType= "application/json"
+)
+
+response_body = json.loads(response.get("body").read())
+response_text= response_body.get("completions")[0].get("data").get("text")
+print(response_text)
